50de8baf98be2323687a7dcafbb773404bee7396
kodok-buncit <fajar90alone@gmail.om>
2014-05-06 03:00:44 +0700

    initial commit

---
+++ src/Fitztrev/LaravelHtmlMinify/LaravelHtmlMinifyCompiler.php
@@ -0,0 +1,77 @@
+<?php namespace Fitztrev\LaravelHtmlMinify;
+
+use Illuminate\View\Compilers\BladeCompiler;
+
+class LaravelHtmlMinifyCompiler extends BladeCompiler
+{
+    private $_config;
+
+    public function __construct($config, $files, $cachePath)
+    {
+        parent::__construct($files, $cachePath);
+        
+        $this->_config = $config;
+        // Add Minify to the list of compilers
+        if ($this->_config['enabled'] === true) {
+            $this->compilers[] = 'Minify';
+        }
+
+        // Set Blade contentTags and escapedContentTags
+        $this->setContentTags(
+            $this->_config['blade']['contentTags'][0],
+            $this->_config['blade']['contentTags'][1]
+        );
+
+        $this->setEscapedContentTags(
+            $this->_config['blade']['escapedContentTags'][0],
+            $this->_config['blade']['escapedContentTags'][1]
+        );
+
+    }
+
+    /**
+     * Compile the view at the given path.
+     *
+     * @param  string  $path
+     * @return void
+     */
+    public function compile($path = null)
+    {
+        if ($path)
+        {
+            $this->setPath($path);
+        }
+
+        $contents = $this->compileMinify( $this->compileString($this->files->get($this->getPath())) );
+
+        if ( ! is_null($this->cachePath))
+        {
+            $this->files->put($this->getCompiledPath($this->getPath()), $contents );
+        }
+    }
+
+    /**
+    * Compress the HTML output before saving it
+    *
+    * @param string $value the contents of the view file
+    *
+    * @return string
+    */
+    protected function compileMinify($value)
+    {
+        $replace = array(
+            '/<!--[^\[](.*?)[^\]]-->/s' => '',
+            "/<\?php/"                  => '<?php ',
+            "/\n([\S])/"                => ' $1',
+            "/\r/"                      => '',
+            "/\n/"                      => '',
+            "/\t/"                      => ' ',
+            "/ +/"                      => ' ',
+        );
+
+        return preg_replace(
+            array_keys($replace), array_values($replace), $value
+        );
+    }
+
+}
---
+++ src/Fitztrev/LaravelHtmlMinify/LaravelHtmlMinifyServiceProvider.php
@@ -0,0 +1,54 @@
+<?php 
+namespace Fitztrev\LaravelHtmlMinify;
+
+use Illuminate\View\ViewServiceProvider as ViewServiceProvider;
+use Illuminate\View\Engines\CompilerEngine;
+class LaravelHtmlMinifyServiceProvider extends ViewServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('fitztrev/laravel-html-minify');
+    }
+
+    /**
+     * Register the Blade engine implementation.
+     *
+     * @param  \Illuminate\View\Engines\EngineResolver  $resolver
+     * @return void
+     */
+    public function registerBladeEngine($resolver)
+    {
+        $app = $this->app;
+
+        // The Compiler engine requires an instance of the CompilerInterface, which in
+        // this case will be the Blade compiler, so we'll first create the compiler
+        // instance to pass into the engine so it can compile the views properly.
+        $app->bindShared('blade.compiler', function($app)
+        {
+            $cache = $app['path.storage'].'/views';
+
+            return new LaravelHtmlMinifyCompiler(
+                $app['config']->get('laravel-html-minify::views'),
+                $app['files'], 
+                $cache
+            );
+        });
+
+        $resolver->register('blade', function() use ($app)
+        {
+            return new CompilerEngine($app['blade.compiler'], $app['files']);
+        });
+    }
+}
